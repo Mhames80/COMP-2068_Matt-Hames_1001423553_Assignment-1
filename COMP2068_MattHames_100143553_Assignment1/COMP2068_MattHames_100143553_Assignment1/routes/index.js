5045e71c14698e05cc7eff0d339189d7b59158e3
Matt <mhames80@gmail.com>
2020-06-24 11:29:51 -0400

    2nd created routes in index.js to 5 veiws and created 5 basic views

--- COMP2068_MattHames_100143553_Assignment1/COMP2068_MattHames_100143553_Assignment1/routes/index.js
+++ COMP2068_MattHames_100143553_Assignment1/COMP2068_MattHames_100143553_Assignment1/routes/index.js
@@ -4,7 +4,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Home' });
+});
+router.get('/about/', function (req, res) {
+    res.render('about', { title: 'About Me' });
+});
+router.get('/projects/', function (req, res) {
+    res.render('projects', { title: 'Projects' });
+});
+router.get('/service/', function (req, res) {
+    res.render('service', { title: 'Service' });
+});
+router.get('/contact/', function (req, res) {
+    res.render('contact', { title: 'Contact Me' });
 });
 
 module.exports = router;
