bfdb87f83d12c0f896eb1656d21f7f359efee3a0
Matt <mhames80@gmail.com>
2020-06-24 12:31:49 -0400

    3rd created a header that links all pages together

--- COMP2068_MattHames_100143553_Assignment1/COMP2068_MattHames_100143553_Assignment1/routes/index.js
+++ COMP2068_MattHames_100143553_Assignment1/COMP2068_MattHames_100143553_Assignment1/routes/index.js
@@ -4,19 +4,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Home' });
+    res.render('index', { title: 'Home ' });
 });
-router.get('/about/', function (req, res) {
-    res.render('about', { title: 'About Me' });
+router.get('/about', function (req, res) {
+    res.render('about', { title: 'About Me ' });
 });
-router.get('/projects/', function (req, res) {
-    res.render('projects', { title: 'Projects' });
+router.get('/projects', function (req, res) {
+    res.render('projects', { title: 'Projects ' });
 });
-router.get('/service/', function (req, res) {
-    res.render('service', { title: 'Service' });
+router.get('/service', function (req, res) {
+    res.render('service', { title: 'Service ' });
 });
-router.get('/contact/', function (req, res) {
-    res.render('contact', { title: 'Contact Me' });
+router.get('/contact', function (req, res) {
+    res.render('contact', { title: 'Contact Me ' });
 });
 
 module.exports = router;
